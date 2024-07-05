801573739fcfebfa135ab4f63da767cbe5383e20
pchai <prempreychhay@gmail.com>
2024-07-05 09:00:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Displaying the pattern
+            Console.WriteLine("   ^   ");
+            Console.WriteLine("  ^^^  ");
+            Console.WriteLine(" ^^^^^ ");
+
+            // Keep console window open until a key is pressed
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
